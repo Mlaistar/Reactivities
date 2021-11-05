30480beabf160ee1af2c6436d66a831378506e9f
Mlaistar <Mlaistarr@gmail.com>
2021-11-05 10:49:07 +0200

    End of section 7 v3

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -16,7 +16,35 @@ function App() {
         <Container style={{marginTop: '7em'}}>
             {/* <Route path='/' component={HomePage} />
             <Route path='/activities' component={ActivityDashboard} />
-            <Route path='/createActivity' component={ActivityForm} /> */}
+            <Route path='/createActivity' component={ActivityForm} /> *
+            
+            
+# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+            
+            
+            /}
             <ActivityDashboard />
         </Container>
     </>
